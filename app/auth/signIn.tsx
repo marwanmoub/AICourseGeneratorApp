100715f089aca35440c45c9f56b7ec5c5abb2aaa
marwanmoub <moubayedmarwancoding@gmai.com>
2025-04-17 20:00:47 +0300

    Create AuthForm for both SignIn and SignUp pages

--- app/auth/signIn.tsx
+++ app/auth/signIn.tsx
@@ -1,8 +1,9 @@
 import { View, Text, Image } from "react-native";
 import React from "react";
+import AuthForm from "@/components/FormComponents/AuthForm";
 
 const SignIn = () => {
-  return <View></View>;
+  return <AuthForm type="sign-in" />;
 };
 
 export default SignIn;
