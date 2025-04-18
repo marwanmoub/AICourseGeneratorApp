65ad272f1d0c92b828fbf155eb49adb82ce9bb1d
marwanmoub <moubayedmarwancoding@gmai.com>
2025-04-18 12:40:11 +0300

    Tabs layout

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,16 +1,25 @@
 import { auth, db } from "@/config/firebaseConfig";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import Toast from "react-native-toast-message";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { SignUpSchema } from "../validation/auth.validation";
-import { doc, setDoc } from "firebase/firestore";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { ToastAndroid } from "react-native";
 
-interface SignUpData {
+interface SignInData {
   email: string;
-  fullName: string;
   password: string;
+}
+
+interface SignUpData extends SignInData {
+  fullName: string;
 }
 
 export const CreateNewUser = async (data: SignUpData) => {
   try {
+    let userResult: any;
     const validatedData = SignUpSchema.parse(data);
     if (validatedData) {
       await createUserWithEmailAndPassword(
@@ -19,12 +28,12 @@ export const CreateNewUser = async (data: SignUpData) => {
         validatedData.password
       ).then(async (userCredential: any) => {
         const user = userCredential.user;
-        await SaveUser(user, validatedData);
+        userResult = await SaveUser(user, validatedData);
       });
 
       return {
         success: true,
-        data: validatedData,
+        data: userResult?.data,
       };
     }
   } catch (err) {
@@ -34,10 +43,67 @@ export const CreateNewUser = async (data: SignUpData) => {
 };
 
 export const SaveUser = async (user: any, signUpData: SignUpData) => {
-  await setDoc(doc(db, "users", signUpData.email), {
+  const data = {
     name: signUpData.fullName,
     email: signUpData.email,
     member: false,
     uid: user?.uid,
-  });
+  };
+  await setDoc(doc(db, "users", signUpData.email), data);
+
+  return {
+    data,
+  };
+};
+
+export const userSignIn = async (data: SignInData) => {
+  try {
+    const response = await signInWithEmailAndPassword(
+      auth,
+      data?.email,
+      data?.password
+    );
+
+    Toast.show({
+      type: "success",
+      text1: "Success",
+      text2: "Signed In Successfully",
+      visibilityTime: 2000,
+    });
+    const user = response.user;
+    return {
+      success: true,
+      data: user,
+    };
+  } catch (err) {
+    Toast.show({
+      type: "error",
+      text1: "Incorrect Email or Password",
+      text2: "Sign In Failed",
+      visibilityTime: 3000,
+    });
+    return {
+      success: false,
+      data: null,
+    };
+  }
+};
+
+export const getUserDetails = async (email: string) => {
+  try {
+    const result = await getDoc(doc(db, "users", email));
+    if (result) {
+      return {
+        success: true,
+        data: result?.data(),
+      };
+    }
+
+    return {
+      success: false,
+      data: null,
+    };
+  } catch (err) {
+    console.error(err);
+  }
 };
