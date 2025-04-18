d9bf74fd92b26560e3ade45e6f6e230dd5c8b3e2
marwanmoub <moubayedmarwancoding@gmail.com>
2025-04-18 14:11:38 +0300

    No Courses page if user is new or has no courses

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,10 +1,21 @@
-import { View, Text } from "react-native";
+import { View, Text, Platform } from "react-native";
 import React from "react";
+import Header from "@/components/Home/Header";
+import Colors from "@/constants/Colors";
+import NoCourse from "@/components/Home/NoCourse";
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View
+      style={{
+        padding: 25,
+        paddingTop: Platform.OS === "ios" && 45,
+        flex: 1,
+        backgroundColor: Colors.WHITE,
+      }}
+    >
+      <Header />
+      <NoCourse />
     </View>
   );
 };
